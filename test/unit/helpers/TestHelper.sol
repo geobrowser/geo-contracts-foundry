4fdd894d4c1db0d874924e05996195550f6b47b0
0xJabberwock <0xjabberwock@defi.sucks>
2025-11-05 15:35:41 -0300

    test: mock and expect

---
+++ test/unit/helpers/TestHelper.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.17;
+
+import {Test} from 'forge-std/Test.sol';
+
+abstract contract TestHelper is Test {
+  /// @notice Helper function to mock and expect calls
+  function _mockAndExpect(address _target, bytes memory _call, bytes memory _returnData) internal {
+    vm.mockCall(_target, _call, _returnData);
+    vm.expectCall(_target, _call);
+  }
+}
