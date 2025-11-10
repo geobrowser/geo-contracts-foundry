cd07b1422ca0d9dd93b875ed58a075535523b570
0xJabberwock <0xjabberwock@defi.sucks>
2025-11-10 15:28:37 -0300

    refactor: declare actions constants globally

--- src/interfaces/ISpaceRegistry.sol
+++ src/interfaces/ISpaceRegistry.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.30;
 
-import {IActionConstants} from 'interfaces/IActionConstants.sol';
 import {ISemver} from 'interfaces/ISemver.sol';
 
 /**
