f7f6fd905591726295615708d90caf383c5c484c
0xJabberwock <0xjabberwock@defi.sucks>
2025-11-12 15:45:10 -0300

    feat: remove legacy files

--- src/interfaces/conditions/IMemberAccessExecuteCondition.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.8.30;
-
-import {IPermissionCondition} from '@aragon/osx/core/permission/IPermissionCondition.sol';
-
-/// @notice The condition associated with `TestSharedPlugin`
-interface IMemberAccessExecuteCondition is IPermissionCondition {}
