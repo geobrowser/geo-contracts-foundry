1470d600164464f0ef559c4c0912e1643279c3d7
0xJabberwock <0xjabberwock@defi.sucks>
2025-12-11 03:36:40 -0300

    feat: deploy space factories

--- script/DeployGEOBrowser.s.sol
+++ script/DeployGEOBrowser.s.sol
@@ -5,14 +5,21 @@ import {Script} from 'forge-std/Script.sol';
 
 import {UnsafeUpgrades} from '@openzeppelin/foundry-upgrades/Upgrades.sol';
 
+import {DAOSpaceFactory} from 'contracts/DAOSpaceFactory.sol';
 import {SpaceRegistry} from 'contracts/SpaceRegistry.sol';
-import {ISpaceRegistry} from 'interfaces/ISpaceRegistry.sol';
+import {VerifierSpaceFactory} from 'contracts/VerifierSpaceFactory.sol';
 
 import 'script/Constants.s.sol' as Constants;
 
 contract DeployGEOBrowser is Script {
   SpaceRegistry public spaceRegistryImplementation;
   SpaceRegistry public spaceRegistryProxy;
+
+  DAOSpaceFactory public daoSpaceFactoryImplementation;
+  DAOSpaceFactory public daoSpaceFactoryProxy;
+
+  VerifierSpaceFactory public verifierSpaceFactoryImplementation;
+  VerifierSpaceFactory public verifierSpaceFactoryProxy;
 
   function setUp() public virtual {}
 
@@ -21,12 +28,28 @@ contract DeployGEOBrowser is Script {
 
     // Deploy the implementation contracts
     spaceRegistryImplementation = new SpaceRegistry();
+    daoSpaceFactoryImplementation = new DAOSpaceFactory();
+    verifierSpaceFactoryImplementation = new VerifierSpaceFactory();
 
     // Deploy and initialize the proxy contracts
     spaceRegistryProxy = SpaceRegistry(
       UnsafeUpgrades.deployUUPSProxy(
         address(spaceRegistryImplementation),
-        abi.encodeCall(ISpaceRegistry.initialize, (Constants.GEO_GENESIS_GEO_MULTISIG_COUNCIL))
+        abi.encodeCall(SpaceRegistry.initialize, (Constants.GEO_GENESIS_GEO_MULTISIG_COUNCIL))
+      )
+    );
+    daoSpaceFactoryProxy = DAOSpaceFactory(
+      UnsafeUpgrades.deployUUPSProxy(
+        address(daoSpaceFactoryImplementation),
+        abi.encodeCall(DAOSpaceFactory.initialize, (spaceRegistryProxy, Constants.GEO_GENESIS_GEO_MULTISIG_COUNCIL))
+      )
+    );
+    verifierSpaceFactoryProxy = VerifierSpaceFactory(
+      UnsafeUpgrades.deployUUPSProxy(
+        address(verifierSpaceFactoryImplementation),
+        abi.encodeCall(
+          VerifierSpaceFactory.initialize, (spaceRegistryProxy, Constants.GEO_GENESIS_GEO_MULTISIG_COUNCIL)
+        )
       )
     );
 
