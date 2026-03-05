3fd08422ab1812d3da3078eeec9e9bdac4b564d2
Yaco 0x <yaco@defi.sucks>
2026-03-05 18:37:20 +0100

    chore: complete warning message on beta contracts

--- src/contracts/DAOSpace.sol
+++ src/contracts/DAOSpace.sol
@@ -16,7 +16,7 @@ import 'src/ActionsConstants.sol' as ActionsConstants;
  * @dev This contract allows members and editors to create proposals, vote, and execute them.
  *      This contract also implements a dual-path governance: fast path (threshold-based, immediate execution)
  *      and slow path (majority voting with voting window). Fast path escalates to slow path on "No" vote.
- * @custom:security WARNING: This contract has not been audited and should not be used to hold funds.
+ * @custom:security WARNING: This contract has not been audited, may contain bugs, and should not be used to hold funds.
  */
 contract DAOSpace is SpaceAccessControl, IDAOSpace {
   /// @inheritdoc IDAOSpace
