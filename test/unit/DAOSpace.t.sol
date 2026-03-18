d00e7baceebddc477cce0528f7b6db2ad3598bfd
0xJabberwock <0xjabberwock@defi.sucks>
2026-03-18 16:43:35 -0300

    test: better proposal setup

--- test/unit/DAOSpace.t.sol
+++ test/unit/DAOSpace.t.sol
@@ -813,7 +813,7 @@ contract UnitDAOSpace is TestHelper {
       _latestProposalVersion,
       _initialEditorASpaceId,
       block.timestamp,
-      block.timestamp - 1,
+      block.timestamp + 1,
       IDAOSpace.VotingMode(_votingMode),
       1,
       1,
