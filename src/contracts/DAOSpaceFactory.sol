4d38ccb3b4c202a2e935f4a282af3b1967e5c97a
Cooki0x <cooki@wonderland.xyz>
2026-06-02 08:14:15 +0100

    chore: bump solidity version

--- src/contracts/DAOSpaceFactory.sol
+++ src/contracts/DAOSpaceFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.8.30;
+pragma solidity 0.8.35;
 
 import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import {UUPSUpgradeable} from '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
--- test/integration/IntegrationUpgradeUUPSImplementation.t.sol
+++ test/integration/IntegrationUpgradeUUPSImplementation.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.8.30;
+pragma solidity 0.8.35;
 
 import {ERC1967Utils} from '@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol';
 
